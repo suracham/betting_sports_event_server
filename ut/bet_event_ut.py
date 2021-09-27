715ea137bc010e3e06d96d4cbfe977d87d973123
Suneel Rachamallu <suneel.rachamallu@ericsson.com>
2021-09-27 15:06:27 +0200

    Adding Sports Betting Platform Server API

--- ut/bet_event_ut.py
+++ ut/bet_event_ut.py
@@ -10,17 +10,17 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
     def setUp(self):
        self.maxDiff = None
        # No check added since UT expects the test profiles available
-       with open('new_event.json') as f:
+       with open('./new_event.json') as f:
          self.test_new_event = json.load(f)
 
-       with open('new_event_2.json') as f:
+       with open('./new_event_2.json') as f:
          self.test_new_event_2 = json.load(f)
 
-       with open('update_odds.json') as f:
+       with open('./update_odds.json') as f:
          self.test_update_odds = json.load(f)
 
     def test_1_post_event_1(self):
-        url = 'http://10.105.159.173:5000/api/match/createevent'
+        url = 'http://127.0.0.1:1234/api/match/createevent'
         data = urllib.urlencode(self.test_new_event)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'POST'
@@ -29,7 +29,7 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
         self.assertEqual(ast.literal_eval(response), {'status': 'Created Event'})
 
     def test_2_post_event_2(self):
-        url = 'http://10.105.159.173:5000/api/match/createevent'
+        url = 'http://127.0.0.1:1234/api/match/createevent'
         data = urllib.urlencode(self.test_new_event_2)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'POST'
@@ -38,7 +38,7 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
         self.assertEqual(ast.literal_eval(response), {'status': 'Created Event'})
 
     def test_3_update_odds_event_1(self):
-        url = 'http://10.105.159.173:5000/api/match/updateodds'
+        url = 'http://127.0.0.1:1234/api/match/updateodds'
         data = urllib.urlencode(self.test_update_odds)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'PUT'
@@ -51,7 +51,7 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
         validation_checks = {self.test_new_event['event']['id']: self.test_update_odds['event'],
                              self.test_new_event_2['event']['id']: self.test_new_event_2['event']}
         for id in [self.test_new_event['event']['id'], self.test_new_event_2['event']['id']]:
-          url = 'http://10.105.159.173:5000/api/match/%s'%(id)
+          url = 'http://127.0.0.1:1234/api/match/%s'%(id)
           data = urllib.urlencode(self.test_update_odds)
           req = urllib2.Request(url, data)
           req.get_method = lambda: 'GET'
@@ -61,8 +61,8 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
           self.assertEqual(response, validation_checks[id])
 
     def test_5_get_data_by_startTime(self):
-        valid_res = '[\n  {\n    "id": 994839351840, \n    "name": "Cavaliers vs Lakers", \n    "startTime": "2018-01-15 22:00:00", \n    "url": "http://10.105.159.173:5000/api/match/994839351840"\n  }, \n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2018-06-20 10:30:00", \n    "url": "http://10.105.159.173:5000/api/match/994839351740"\n  }\n]\n'
-        url = 'http://10.105.159.173:5000/api/match?ordering=startTime'
+        valid_res = '[\n  {\n    "id": 994839351840, \n    "name": "Cavaliers vs Lakers", \n    "startTime": "2021-01-15 22:00:00", \n    "url": "http://127.0.0.1:1234/api/match/994839351840"\n  }, \n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2021-06-20 10:30:00", \n    "url": "http://127.0.0.1:1234/api/match/994839351740"\n  }\n]\n'
+        url = 'http://127.0.0.1:1234/api/match?ordering=startTime'
         data = urllib.urlencode(self.test_update_odds)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'GET'
@@ -71,8 +71,8 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
         self.assertEqual(response, valid_res)
 
     def test_6_get_data_by_id(self):
-        valid_res = '[\n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2018-06-20 10:30:00", \n    "url": "http://10.105.159.173:5000/api/match/994839351740"\n  }, \n  {\n    "id": 994839351840, \n    "name": "Cavaliers vs Lakers", \n    "startTime": "2018-01-15 22:00:00", \n    "url": "http://10.105.159.173:5000/api/match/994839351840"\n  }\n]\n'
-        url = 'http://10.105.159.173:5000/api/match?ordering=id'
+        valid_res = '[\n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2021-06-20 10:30:00", \n    "url": "http://127.0.0.1:1234/api/match/994839351740"\n  }, \n  {\n    "id": 994839351840, \n    "name": "Cavaliers vs Lakers", \n    "startTime": "2021-01-15 22:00:00", \n    "url": "http://127.0.0.1:1234/api/match/994839351840"\n  }\n]\n'
+        url = 'http://127.0.0.1:1234/api/match?ordering=id'
         data = urllib.urlencode(self.test_update_odds)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'GET'
@@ -81,8 +81,8 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
         self.assertEqual(response, valid_res)
 
     def test_7_get_data_by_keys(self):
-        valid_res = '[\n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2018-06-20 10:30:00", \n    "url": "http://10.105.159.173:5000/api/match/994839351740"\n  }\n]\n'
-        url = 'http://10.105.159.173:5000/api/match/?name=Real%20Madrid%20vs%20Barcelona'
+        valid_res = '[\n  {\n    "id": 994839351740, \n    "name": "Real Madrid vs Barcelona", \n    "startTime": "2021-06-20 10:30:00", \n    "url": "http://127.0.0.1:1234/api/match/994839351740"\n  }\n]\n'
+        url = 'http://127.0.0.1:1234/api/match/?name=Real%20Madrid%20vs%20Barcelona'
         data = urllib.urlencode(self.test_update_odds)
         req = urllib2.Request(url, data)
         req.get_method = lambda: 'GET'
@@ -94,7 +94,7 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
     def test_8_delete_event(self):
      # Delete Events
      for id in [self.test_new_event['event']['id'], self.test_new_event_2['event']['id']]:
-       url = 'http://10.105.159.173:5000/api/match/deleteevent/%s'%(id)
+       url = 'http://127.0.0.1:1234/api/match/deleteevent/%s'%(id)
        req = urllib2.Request(url)
        req.get_method = lambda: 'DELETE'
        response = urllib2.urlopen(req).read()
